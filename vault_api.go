c65b1969351bbcd23882a36f102e67abe8425298
rowetechinc <rcastelo@rowetechinc.com>
2019-03-15 11:18:19 -0700

    Fixing bugs

--- vault_api.go
+++ vault_api.go
@@ -594,7 +594,7 @@ func vaultAPISnrTestGetHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Replace all NaN with Null
 	fmt.Println("SNR Contains NaN", snrTestData)
-	snrTestData = strings.Replace(snrTestData, "NaN", "null", -1) 
+	snrTestData = strings.Replace(&snrTestData, "NaN", "null", -1) 
 	fmt.Println("SNR Contains NaN", snrTestData)
 
 	if err := json.NewEncoder(w).Encode(snrTestData); err != nil {
